c3bd1e40efee7b6abe71691083ed4335bb2dc72a
MarioMartinez00072520 <00072520@uca.edu.sv>
2023-11-05 20:29:34 -0600

    :card_file_box: add(database/config): Added database config for db connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import configuration from "./config/configuration";
+
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env','.env.development.local', '.env.development'],
+      load: [configuration]
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('database.uri'),
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
