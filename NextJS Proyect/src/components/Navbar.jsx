66f6469814e6ca7276b5e41a1963d88e9154addd
VictorCortez358 <00022120@uca.edu.sv>
2023-11-09 09:47:37 -0600

    ➕ add(dependency): Added IconsAntDesign

--- NextJS Proyect/src/components/Navbar.jsx
+++ NextJS Proyect/src/components/Navbar.jsx
@@ -1,15 +1,42 @@
-import { Button } from 'antd';
+import { Button, ConfigProvider } from 'antd';
 import Image from 'next/image';
+import LogoSharedView from '../../public/ICONO.png';
 
 const Navbar = () => {
+
+    const getDate = () => {
+        const date = new Date();
+        const day = date.getDate();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        const hour = date.getHours();
+        const minutes = date.getMinutes();
+        return `${hour}:${minutes} - ${day}/${month}/${year}`;
+    }
+
     return(
-        <div className="flex flex-row items-center">
-            <div className='w-1/2 flex justify-start'>
-                <span>SharedView</span>
+        <div className="flex flex-row items-center px-8 py-4">
+            <div className='w-1/2 flex justify-start items-center gap-4'>
+                <Image src={LogoSharedView} width={60} height={60} />
+                <div className='flex flex-row gap-1'>
+                    <span className='text-violet-700 font-bold text-lg'>Shared</span>
+                    <span className='font-bold text-lg'>View</span>
+                </div>
             </div>
-            <div className='w-1/2 flex flex-row items-center'>
-                <span>Time: </span>
+            <div className='w-1/2 flex flex-row items-center justify-end gap-4'>
+                <span className='text-gray-400 text-sm'>{getDate()}</span>
+                <ConfigProvider
+                            theme={{
+                                token:{
+                                    colorText: "#811cb7",
+                                    colorBorder: "#811cb7",
+                                    colorBgTextHover: "#fff",
+                                    colorPrimary: "#fff",
+                                }
+                            }}
+                >
                 <Button>Login</Button>
+                </ConfigProvider>
             </div>
         </div>
     )
