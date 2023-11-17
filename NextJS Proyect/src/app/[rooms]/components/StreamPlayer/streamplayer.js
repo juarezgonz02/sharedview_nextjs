ef4bcf6d6be70522880f5cc768dde03963e78dce
Oscar Juarez <00126320@uca.edu.sv>
2023-11-17 00:04:51 -0600

    ⚡️Improved(stream player): Now uses flv.js

--- NextJS Proyect/src/app/[rooms]/components/StreamPlayer/streamplayer.js
+++ NextJS Proyect/src/app/[rooms]/components/StreamPlayer/streamplayer.js
@@ -1,85 +1,63 @@
 import React, { useEffect, useRef, useState } from 'react'
-import Hls from 'hls.js';
+import flv from 'flv.js'
+import { env } from '../../../../../next.config';
+import { useParams, useSearchParams } from 'next/navigation';
+import StreamInstruction from './StreamInstruction';
+import Cookies from 'js-cookie'
 
 const StreamPlayer = () => {
-    
-    const [loaded, setLoaded] = useState(false)
-    const videoPlayer = useRef()
 
-    const videoSrc = 'https://cph-msl.akamaized.net/hls/live/2000341/test/master.m3u8';
-    //const videoSrc = 'http://live.sharedview.tk:3000/hls/web.m3u8';
-    // Before check for m3u8 data
-    
-    
-    const get_m3u8 = async () => {
-        let testing;
-        
-        let fetching = setInterval(async () =>
-            {
-                testing = await fetch(videoSrc,
-                {   
-                    method: 'GET',
-                    mode: 'cors',
-                })
-                
-                console.log(testing.status);
+        const [loaded, setLoaded] = useState(false)
 
-                if(testing.status === 200){
-                    hls_play();
-                    clearInterval(fetching);
-                    setLoaded(true)
+        const videoPlayerElement = useRef();
+
+        const { rooms } = useParams()
+
+        const videoSrc = `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_HOST}:${process.env.NEXT_PUBLIC_HLS_PORT}/live/${rooms}.flv` +
+            `?sign=${Cookies.get('room-exp')}-${Cookies.get('sign')}`;
+
+        const play_stream = async () => {
+
+          if (flv.isSupported()) {
+
+
+              const flvPlayer = flv.createPlayer({
+                  type: 'flv',
+                  url: videoSrc,
+                  isLive: true
+              });
+
+              flvPlayer.on(flv.Events.METADATA_ARRIVED, ()=>{
+                  setLoaded(true)
+                  videoPlayerElement.current.className  = "loaded-stream-video"
+              })
+
+              flvPlayer.attachMediaElement(videoPlayerElement.current);
+              flvPlayer.load();
+              flvPlayer.play()
+
+          }
+        }
+
+        useEffect(() => {
+            play_stream()
+        }, [])
+
+        return (
+            <div className="video-container">
+
+                {
+                    !loaded &&
+                    <StreamInstruction />
                 }
-            
-            }, 1000)
-                
-    
-    }
-        //
-    const hls_play = () => {
-    
-        //
-        // First check for native browser HLS support
-        //
-        console.log()
-        if(videoPlayer.current.canPlayType('application/vnd.apple.mpegurl')) {
-            videoPlayer.current.src = videoSrc;
-            videoPlayer.current.addEventListener('loadedmetadata', function() {
-                videoPlayer.current.play();
-                });
-                //
-        // If no native HLS support, check if hls.js is supported
-        //
-        }else if (Hls.isSupported()) {
-            
-            const hls = new Hls();
-            try{
-                hls.loadSource(videoSrc);
-            }catch(e){
-                console.log("NO HAY, NO EXISTE")
-            }
-            hls.attachMedia(videoPlayer.current);
-            hls.on(Hls.Events.MANIFEST_PARSED, function() {
-                //videoPlayer.current.play();
-                videoPlayer.current.id="video-stream";
-            });
-        } 
-    }
-        
-    useEffect(()=>{
-        console.log(videoPlayer.current)
-        get_m3u8()
-    },[])
-    
-    
-  return (
-    <div className="video-container">
 
-    <div id="video-stream-cont">
-        <video autoPlay controls className="stream-video" ref={videoPlayer} ></video>
+                <div id="video-stream-cont">
 
-    </div>
+                     <video autoPlay controls className={"stream-video"} ref={videoPlayerElement} ></video>
 
-  </div>  )
+                </div>
+
+            </div>)
 }
 
 export default StreamPlayer
