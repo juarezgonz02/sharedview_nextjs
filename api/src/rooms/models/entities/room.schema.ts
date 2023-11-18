f6e0d4832625ba29985e0991695833dacca93fa2
MarioMartinez00072520 <00072520@uca.edu.sv>
2023-11-18 12:28:16 -0600

    :recycle: update(room/mode'): added fields to schema

--- api/src/rooms/models/entities/room.schema.ts
+++ api/src/rooms/models/entities/room.schema.ts
@@ -1,6 +1,9 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { User } from "../../../users/models/entities/user.schema";
-import mongoose from "mongoose";
+import mongoose, { HydratedDocument } from "mongoose";
+import { genRoomCode } from "src/rooms/utils/room.utils";
+
+export type RoomDocument = HydratedDocument<Room>;
 
 @Schema({
     timestamps: true,
@@ -8,12 +11,18 @@ import mongoose from "mongoose";
 export class Room{
     @Prop({ required: true})
     code: string;
-    @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User"}], required: true})
-    owner: User;
+    @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId,  ref: "User"}], required: true})
+    owner: string;
     @Prop()
     name: string;
-    @Prop({ default: true})
+    @Prop({ default: true })
     isPublic: boolean;
     @Prop({ default: []})
-    accessUsers: User[]
+    accessUsers: User[];
+    @Prop({ default: new Date() })
+    creationDate: Date;
+    @Prop()
+    expirationDate: Date;
 }
+
+export const RoomSchema = SchemaFactory.createForClass(Room);
