99d44feb5305d5e0f1b9157d7fe9838ed0bb1eff
Oscar Juarez <00126320@uca.edu.sv>
2023-11-22 23:40:59 -0600

    🚑 Fix: Styles not showing in rooms

--- NextJS Proyect/src/app/[rooms]/page.js
+++ NextJS Proyect/src/app/[rooms]/page.js
@@ -3,6 +3,7 @@ import Chat from "./components/ChatComponent/chat";
 import SocketContext from "./components/socketContext";
 import StreamPlayer from "./components/StreamPlayer/streamplayer";
 import CallInterface from "./components/CallInterface/callInterface";
+import "./main.css"
 
 export default function Page({ params }) {
 
